068272fa3623e7b02be085411fbb426d03fa6415
Jan Gottweiss <jan.gottweiss@sap.com>
2019-08-19 11:05:54 +0200

    add build process

---
+++ src/index.js
@@ -0,0 +1,2 @@
+const MAPBOX_TOKEN = process.env.MapboxAccessToken;
+const WARNING_MESSAGE = 'Please Provide a Mapbox Token in order to use Kepler.gl. Edit this file and fill out MAPBOX_TOKEN with your access key';
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin(['MapboxAccessToken']),
+    new HtmlWebpackPlugin({
+        // injects bundle.js to our new index.html
+        inject: false,
+        // copys the content of the existing index.html to the new /build index.html
+        template:  path.resolve('./index.html'),
+      }),
+  ]
+};
